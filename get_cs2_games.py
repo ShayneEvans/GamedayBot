fbd5f8857a64d2e73961735ffd0597dad9b21af8
Ubuntu <ubuntu@ip-172-31-19-187.us-east-2.compute.internal>
2023-11-07 17:46:56 -0500

    Added try/except/finally block to 2 selenium scripts, removed print

--- get_cs2_games.py
+++ get_cs2_games.py
@@ -16,6 +16,8 @@ from datetime import datetime
 import psycopg2
 import os
 import sys
+import time
+from datetime import datetime
 
 #Used for date conversion to readable string in EST.
 def convert_date(date):
@@ -54,67 +56,68 @@ def get_time_until_match(game_start_time):
 
 #Used to scrape new matches
 def get_upcoming_matches():
+    upcoming_games = []
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=op)
     url = 'https://www.hltv.org/matches'
     try:
         driver.get(url)
+        all_matches = driver.find_elements(By.CLASS_NAME, "upcomingMatch")
+
+        for match in all_matches:
+            match_url = match.find_element(By.CLASS_NAME, "match.a-reset").get_attribute('href')
+
+            no_game = False
+            #See if match exists yet, for example could be placeholder for a future match such as Final or semi-final
+            #If no exceptions both team names will be obtained.
+            try:
+                #Check to see if team is decided yet (doesn't depend on another match result)
+                try:
+                    team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .matchTeamName").text
+                except NoSuchElementException:
+                    team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .team").text
+
+                #Check to see if team is decided yet (doesn't depend on another match result)
+                try:
+                    team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .matchTeamName").text
+                except NoSuchElementException:
+                    team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .team").text
+
+            except NoSuchElementException:
+                no_game = True
+
+            #Scraping match information for each match
+            if no_game == False:
+                #Getting Team IDs
+                team1_id = match.get_attribute("team1")
+                team2_id = match.get_attribute("team2")
+
+                #Match type: BO1/BO3/BO5
+                match_length = match.find_element(By.CSS_SELECTOR,".matchMeta").text
+
+                #Getting the starting date and time until the game
+                unix_timestamp_start_date = match.get_attribute("data-zonedgrouping-entry-unix")
+                start_time_dt = datetime.fromtimestamp(int(unix_timestamp_start_date)/1000)
+                start_time_str = start_time_dt.strftime("%m-%d-%Y %H:%M")
+                start_time_converted = convert_date(start_time_str)
+
+                #Checkin if LAN or ONLINE game
+                match_env = match.get_attribute("lan")
+                if match_env == "true":
+                    match_env = "LAN"
+                else:
+                    match_env = "ONLINE"
+
+                #Avoiding adding games where team depends on outcome of another match
+                if team1_id is not None and team2_id is not None:
+                    upcoming_games.append((start_time_converted, team1_id, team2_id, match_length.upper(), match_env, match_url))
+    
     except Exception as e:
-        print(f"Error occurred at get: {str(e)}")
-        sys.exit(1)
+        print(f"Error: {str(e)}")
+    
+    finally:
+        if driver is not None:
+            driver.quit()
 
-    all_matches = driver.find_elements(By.CLASS_NAME, "upcomingMatch")
-
-    upcoming_games = []
-
-    for match in all_matches:
-        match_url = match.find_element(By.CLASS_NAME, "match.a-reset").get_attribute('href')
-
-        no_game = False
-        #See if match exists yet, for example could be placeholder for a future match such as Final or semi-final
-        #If no exceptions both team names will be obtained.
-        try:
-            #Check to see if team is decided yet (doesn't depend on another match result)
-            try:
-                team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .matchTeamName").text
-            except NoSuchElementException:
-                team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .team").text
-
-            #Check to see if team is decided yet (doesn't depend on another match result)
-            try:
-                team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .matchTeamName").text
-            except NoSuchElementException:
-                team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .team").text
-
-        except NoSuchElementException:
-            no_game = True
-
-        #Scraping match information for each match
-        if no_game == False:
-            #Getting Team IDs
-            team1_id = match.get_attribute("team1")
-            team2_id = match.get_attribute("team2")
-
-            #Match type: BO1/BO3/BO5
-            match_length = match.find_element(By.CSS_SELECTOR,".matchMeta").text
-
-            #Getting the starting date and time until the game
-            unix_timestamp_start_date = match.get_attribute("data-zonedgrouping-entry-unix")
-            start_time_dt = datetime.fromtimestamp(int(unix_timestamp_start_date)/1000)
-            start_time_str = start_time_dt.strftime("%m-%d-%Y %H:%M")
-            start_time_converted = convert_date(start_time_str)
-
-            #Checkin if LAN or ONLINE game
-            match_env = match.get_attribute("lan")
-            if match_env == "true":
-                match_env = "LAN"
-            else:
-                match_env = "ONLINE"
-
-            #Avoiding adding games where team depends on outcome of another match
-            if team1_id is not None and team2_id is not None:
-                upcoming_games.append((start_time_converted, team1_id, team2_id, match_length.upper(), match_env, match_url))
-
-    driver.close()
     return upcoming_games
 
 #Inserts all new games into database
@@ -131,15 +134,24 @@ def insert_upcoming_matches_to_db(cs2_upcoming_matches):
 
     #If there are upcoming games
     if len(cs2_upcoming_matches) != 0:
-        print(f'Inserting {len(cs2_upcoming_matches)} Games')
+        result_message = f'{len(cs2_upcoming_matches)} Games Insertion Attempted'
         cs2_args_str = ','.join(cur_get_games.mogrify("(%s,%s,%s,%s,%s,%s)", i).decode('utf-8')
                                 for i in cs2_upcoming_matches)
         cur_get_games.execute("INSERT INTO cs2_games VALUES " + (
             cs2_args_str) + " ON CONFLICT(start_time, team_one, team_two) DO NOTHING")
-        conn_get_games.commit()
+        conn_get_games.commit() 
     else:
-        print("No games to insert")
+        result_message = "No New Games to Insert"
+    
+    cur_get_games.close()
+    conn_get_games.close()
+    return result_message
 
 #Getting upcoming matches from https://www.hltv.org/matches and putting new games into database
+start_time = time.time()
 cs2_upcoming_matches = get_upcoming_matches()
-insert_upcoming_matches_to_db(cs2_upcoming_matches)
+insert_result = insert_upcoming_matches_to_db(cs2_upcoming_matches)
+end_time = time.time()
+execution_time = end_time - start_time
+current_time = datetime.now()
+print(f"[{current_time}]: Executed get_cs2_games in {execution_time} seconds, RESULT: {insert_result}") 
--- get_new_teams_from_upcoming_cs2_matches.py
+++ get_new_teams_from_upcoming_cs2_matches.py
@@ -9,82 +9,105 @@ from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 import psycopg2
 import os
+from datetime import datetime
+import time
 op = webdriver.ChromeOptions()
 op.add_argument("--headless=new")
 op.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
 
-#Connecting to database
-conn = psycopg2.connect(
-    database= os.environ.get('GamedayBot_database'),
-    user= os.environ.get('GamedayBot_user'),
-    password= os.environ.get('GamedayBot_password'),
-    host = os.environ.get('GamedayBot_host'),
-    port = os.environ.get('GamedayBot_port'),
-    keepalives=1,
-    keepalives_idle=1
-    )
-
-cur = conn.cursor()
-
 #Gets list of cs2 teams from csv
 def get_cs2_teams_list():
+	#Connecting to database
+    conn = psycopg2.connect(
+        database= os.environ.get('GamedayBot_database'),
+        user= os.environ.get('GamedayBot_user'),
+        password= os.environ.get('GamedayBot_password'),
+        host = os.environ.get('GamedayBot_host'),
+        port = os.environ.get('GamedayBot_port'),
+        keepalives=1,
+        keepalives_idle=1
+    )
+
+    cur = conn.cursor()
+
     select_statement = "SELECT team_id, team_name FROM cs2_teams"
     cur.execute(select_statement)
     cs2_teams_list = cur.fetchall()
+
+    cur.close()
+    conn.close()
 
     return cs2_teams_list
 
 #Inserts newly found teams into database
 def insert_new_teams_to_database(new_cs2_teams_list):
+    pycopg2.connect(
+  		database= os.environ.get('GamedayBot_database'),
+        user= os.environ.get('GamedayBot_user'),
+        password= os.environ.get('GamedayBot_password'),
+        host = os.environ.get('GamedayBot_host'),
+        port = os.environ.get('GamedayBot_port'),
+        keepalives=1,
+        keepalives_idle=1)
+
+    cur = conn.cursor()
     #Batch insert of new teams into database
     cs2_args_str = ','.join(cur.mogrify("(%s,%s,%s,%s)", i).decode('utf-8')
                             for i in new_cs2_teams_list)
     cur.execute("INSERT INTO cs2_teams VALUES " + (
         cs2_args_str) + " ON CONFLICT(team_id) DO NOTHING")
     conn.commit()
-
+    cur.close()
+    conn.close()
 
 #Attempting to find new teams to add to csv from the upcoming matches
 def get_teams_from_upcoming_matches():
-    url = 'https://www.hltv.org/matches'
-    driver.get(url)
-    all_matches = driver.find_elements(By.CLASS_NAME, "upcomingMatch")
-
     upcoming_games_teams = []
+    url = 'https://www.hltv.org/matches'
+    #Initializing driver for webscraping
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=op)
+    
+    try:
+        driver.get(url)
+        all_matches = driver.find_elements(By.CLASS_NAME, "upcomingMatch")
+        for match in all_matches:
 
-    for match in all_matches:
-
-        no_game = False
-        #See if match exists yet, for example could be placeholder for a future match such as Final or semi-final
-        #If no exceptions both team names will be obtained.
-        try:
-            #Check to see if team is decided yet (doesn't depend on another match result)
+            no_game = False
+            #See if match exists yet, for example could be placeholder for a future match such as Final or semi-final
+            #If no exceptions both team names will be obtained.
             try:
-                team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .matchTeamName").text
+                #Check to see if team is decided yet (doesn't depend on another match result)
+                try:
+                    team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .matchTeamName").text
+                except NoSuchElementException:
+                    team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .team").text
+
+                #Check to see if team is decided yet (doesn't depend on another match result)
+                try:
+                    team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .matchTeamName").text
+                except NoSuchElementException:
+                    team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .team").text
 
             except NoSuchElementException:
-                team1_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team1 .team").text
+                no_game = True
 
-            #Check to see if team is decided yet (doesn't depend on another match result)
-            try:
-                team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .matchTeamName").text
+            #Scraping match information for each match
+            if no_game == False:
+                # Getting Team IDs
+                team1_id = match.get_attribute("team1")
+                team2_id = match.get_attribute("team2")
 
-            except NoSuchElementException:
-                team2_name = match.find_element(By.CSS_SELECTOR, ".matchTeam.team2 .team").text
+                if team1_id is not None and team1_id not in {t[0] for t in upcoming_games_teams}:
+                    upcoming_games_teams.append((team1_id, team1_name))
+                if team2_id is not None and team2_id not in {t[0] for t in upcoming_games_teams}:
+                    upcoming_games_teams.append((team2_id, team2_name))
 
-        except NoSuchElementException:
-            no_game = True
-
-        #Scraping match information for each match
-        if no_game == False:
-            # Getting Team IDs
-            team1_id = match.get_attribute("team1")
-            team2_id = match.get_attribute("team2")
-
-            if team1_id is not None and team1_id not in {t[0] for t in upcoming_games_teams}:
-                upcoming_games_teams.append((team1_id, team1_name))
-            if team2_id is not None and team2_id not in {t[0] for t in upcoming_games_teams}:
-                upcoming_games_teams.append((team2_id, team2_name))
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    
+    finally:
+        if driver is not None:
+            driver.quit()
 
     return upcoming_games_teams
 
@@ -102,29 +125,27 @@ def create_insertable_new_teams_list(new_cs2_team_list):
 
     return insertable_cs2_teams_list
 
+start_time = time.time()
 #Getting list of tuples from the database
 cs2_teams = get_cs2_teams_list()
 
-#Initializing driver for webscraping
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=op)
-
 #Getting all teams from upcoming matches
 cs2_teams_from_upcoming_matches = get_teams_from_upcoming_matches()
-driver.close()
 
 #Adding new teams to the original cs2_teams
 new_teams_list = create_new_teams_list(cs2_teams, cs2_teams_from_upcoming_matches)
+end_time = time.time()
+execution_time = end_time - start_time
 
 #New teams have been found
 if len(new_teams_list) > 0:
     #Inserting new teams to database
     insertable_teams_list = create_insertable_new_teams_list(new_teams_list)
     insert_new_teams_to_database(insertable_teams_list)
-    print(insertable_teams_list)
-    print(f"{len(new_teams_list)} New Teams Added")
+    insert_result = f"{len(new_teams_list)} New Teams Added"
 else:
-    print(cs2_teams_from_upcoming_matches)
-    print("No new teams")
+    insert_result = "No new teams added"
 
-cur.close()
-conn.close()
+current_time = datetime.now()
+
+print(f"[{current_time}]: get_new_teams_from_upcoming_cs2_matches Executed in {execution_time}, RESULT: {insert_result}")
